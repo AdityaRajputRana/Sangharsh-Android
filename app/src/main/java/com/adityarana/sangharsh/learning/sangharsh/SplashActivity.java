1e40cee5f15f3dc5c644fc6578f18f7cf0480778
aditya rana <adityarajputrana2016@gmail.com>
2020-08-31 17:00:01 +0530

    Initial commit

---
+++ app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.adityarana.sangharsh.learning.sangharsh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Runnable runnable;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent;
+                if (user != null && user.getUid() != null){
+                   intent = new Intent(SplashActivity.this, MainActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+                }
+
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler = new Handler();
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+}
