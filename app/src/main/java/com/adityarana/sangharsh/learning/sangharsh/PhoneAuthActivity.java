eb28d815b0b32c75d2bd5eb95bdc50c2c51f91bc
aditya rana <adityarajputrana2016@gmail.com>
2021-05-26 20:12:04 +0530

    referral code to phone auth added

--- app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/PhoneAuthActivity.java
+++ app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/PhoneAuthActivity.java
@@ -21,6 +21,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.adityarana.sangharsh.learning.sangharsh.Model.Referral;
 import com.adityarana.sangharsh.learning.sangharsh.Model.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -39,28 +40,40 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class PhoneAuthActivity extends AppCompatActivity {
 
     private TextView infoTxt;
     private Button continueBtn;
+    private Button skipBtn;
     private EditText phoneEditTxt;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
     private boolean isOtpSend = false;
+    private boolean referralUserAdded = false;
     private String mobileNum;
     private String mVerificationID;
     PhoneAuthProvider.ForceResendingToken mResendToken;
+
+    private Boolean isNewUserRegistered = false;
+    private boolean referralAwarded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth);
 
+        skipBtn = findViewById(R.id.skipBtn);
 
         TextView ppTxt = findViewById(R.id.ppTxt);
         ppTxt.setText(Html.fromHtml("By logging in you agree to our <font color=#2948FF>Terms and Conditions</font> and <font color=#2948FF> Privacy Policy </font>"));
@@ -81,7 +94,7 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
 
     private void updateUI(FirebaseUser user) {
-        if (user != null) {
+        if (user != null && isNewUserRegistered && referralAwarded && referralUserAdded) {
             setResult(RESULT_OK);
             finish();
         }
@@ -187,6 +200,7 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
         if (task.getResult().getAdditionalUserInfo().isNewUser()) {
             User user = new User();
+            showReferralEditText();
             user.setUid(task.getResult().getUser().getUid());
             String androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
             user.setLoginUUID(androidId);
@@ -197,13 +211,146 @@ public class PhoneAuthActivity extends AppCompatActivity {
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
+                            isNewUserRegistered = true;
                             updateUI(fuser);
                         }
                     });
         } else {
+            isNewUserRegistered = true;
+            referralAwarded = true;
+            referralUserAdded = true;
             updateUI(fuser);
         }
     }
+
+    private void showReferralEditText() {
+        progressBar.setVisibility(View.GONE);
+        continueBtn.setEnabled(true);
+        phoneEditTxt.setText("");
+        phoneEditTxt.setInputType(InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+        phoneEditTxt.setEnabled(true);
+        infoTxt.setText("If you have any referral Code you may enter here (Optional). If not click the skip button");
+        phoneEditTxt.setHint("Referral Code");
+
+        Button skipBtn = findViewById(R.id.skipBtn);
+        skipBtn.setVisibility(View.VISIBLE);
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                referralAwarded = true;
+                referralUserAdded = true;
+                updateUI(FirebaseAuth.getInstance().getCurrentUser());
+            }
+        });
+
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (phoneEditTxt.getText() != null && !phoneEditTxt.getText().toString().isEmpty()){
+                    verifyReferralId();
+                    disableEverything();
+                }
+            }
+        });
+    }
+
+    private void verifyReferralId() {
+
+        Log.i("MyLogs", "Verifying Now");
+
+        FirebaseDatabase.getInstance()
+                .getReference("referrals")
+                .child(phoneEditTxt.getText().toString().toUpperCase())
+                .child("exists")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists() && snapshot.getValue() != null && snapshot.getValue(Boolean.class)){
+                            startSignUp();
+                        } else {
+                            enableEverything();
+                            phoneEditTxt.setError("This referral code does not exists");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        enableEverything();
+                        Toast.makeText(PhoneAuthActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void startSignUp() {
+        addReferral();
+        addReferredUser();
+    }
+
+    private void addReferredUser() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        User mUser = new User();
+        mUser.setUid(user.getUid());
+        mUser.setReferredBy(phoneEditTxt.getText().toString().toUpperCase());
+        String androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+        mUser.setLoginUUID(androidId);
+        FirebaseDatabase.getInstance()
+                .getReference("users")
+                .child(user.getUid())
+                .setValue(mUser)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        referralUserAdded = true;
+                        if (referralAwarded && isNewUserRegistered){
+                            updateUI(user);
+                        }
+                    }
+                });
+    }
+
+
+    private void addReferral() {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Referral referral = new Referral();
+        referral.setDetails(user.getDisplayName());
+        referral.setPurchaseMade(false);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("timestamp", ServerValue.TIMESTAMP);
+        referral.setJoinedOn(map);
+
+        FirebaseDatabase.getInstance()
+                .getReference("referrals")
+                .child(phoneEditTxt.getText().toString().toUpperCase())
+                .child(user.getUid())
+                .setValue(referral)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        referralAwarded = true;
+                        if (referralUserAdded && isNewUserRegistered){
+                            updateUI(user);
+                        }
+                    }
+                });
+    }
+
+
+
+    private void enableEverything() {
+        progressBar.setVisibility(View.GONE);
+        skipBtn.setEnabled(true);
+        continueBtn.setEnabled(true);
+        phoneEditTxt.setEnabled(true);
+    }
+
+ private void disableEverything() {
+        progressBar.setVisibility(View.VISIBLE);
+        skipBtn.setEnabled(false);
+        continueBtn.setEnabled(false);
+        phoneEditTxt.setEnabled(false);
+    }
+
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
         @Override
@@ -225,9 +372,9 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
             progressBar.setVisibility(View.GONE);
             continueBtn.setEnabled(true);
-            phoneEditTxt.setText("");
             phoneEditTxt.setEnabled(true);
             infoTxt.setText("Enter the OTP sent to your mobile number " + phoneEditTxt.getText().toString());
+            phoneEditTxt.setText("");
             phoneEditTxt.setHint("Enter OTP");
             isOtpSend = true;
         }
