fe7fd0c465331fbb9cfde3ca86e801a55bb9fe76
aditya rana <adityarajputrana2016@gmail.com>
2021-06-03 20:24:58 +0530

    starting

--- app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/NotificationActivity.java
+++ app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/NotificationActivity.java
@@ -1,14 +1,58 @@
 package com.adityarana.sangharsh.learning.sangharsh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void goBack(View view){
+        onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        loadData();
+    }
+
+    private void loadData() {
+        FirebaseDatabase.getInstance().getReference("notifications")
+                .child("all")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()  && snapshot.hasChildren()){
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(NotificationActivity.this, "Some error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
