4bb2bf6f088645f45935a4fe6844279f5317f80d
aditya rana <adityarajputrana2016@gmail.com>
2021-06-04 08:34:34 +0530

    notificationsAdded

---
+++ app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/Adapter/NotificationAdapter.java
@@ -0,0 +1,128 @@
+package com.adityarana.sangharsh.learning.sangharsh.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.adityarana.sangharsh.learning.sangharsh.Model.Notifications.Notification;
+import com.adityarana.sangharsh.learning.sangharsh.R;
+import com.adityarana.sangharsh.learning.sangharsh.Tools.RoundedCornersTransformation;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder> {
+
+    private ArrayList<Notification> notifications;
+    private Date date;
+
+    public NotificationAdapter(ArrayList<Notification> notifications) {
+        this.notifications = notifications;
+        date = new Date();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Notification mNotif = notifications.get(position);
+
+        if (mNotif.getTitle() != null && !mNotif.getTitle().isEmpty()){
+            holder.titleTxt.setText(mNotif.getTitle());
+        } else {
+            holder.titleTxt.setVisibility(View.GONE);
+        }
+
+        if (mNotif.getMessage() != null && !mNotif.getMessage().isEmpty()){
+            holder.messageTxt.setText(mNotif.getMessage());
+        } else {
+            holder.messageTxt.setVisibility(View.GONE);
+        }
+
+        if (mNotif.getIcon() != null && !mNotif.getIcon().isEmpty()){
+            Picasso.get()
+                    .load(mNotif.getIcon())
+                    .transform(new CropCircleTransformation())
+                    .into(holder.icon);
+        } else {
+            Picasso.get()
+                    .load(R.mipmap.ic_launcher)
+                    .transform(new CropCircleTransformation())
+                    .into(holder.icon);
+        }
+
+        if (mNotif.getImage() != null && !mNotif.getImage().isEmpty()){
+            Picasso.get()
+                    .load(mNotif.getImage())
+                    .transform(new RoundedCornersTransformation(25, 20))
+                    .into(holder.imageView);
+        } else {
+            holder.imageView.setVisibility(View.GONE);
+        }
+
+        if (mNotif.getTitle() != null && mNotif.getTime().containsKey("createdAt")){
+            Date createdAt = new Date((long) mNotif.getTime().get("createdAt"));
+
+            long difference = date.getTime() -  createdAt.getTime();
+
+            int seconds = (int) (difference/1000);
+            int minutes = (int) (seconds/60);
+            int hours = (int) (minutes/60);
+
+            int days = (int) (hours/24);
+            int months = (int) (days/30.5);
+            int years = (int) (months/12);
+
+            if (seconds<60){
+                holder.timeTxt.setText(String.valueOf(seconds) + "seconds");
+            } else if (minutes<60){
+                holder.timeTxt.setText(String.valueOf(minutes) + "minutes");
+            } else if (hours<24){
+                holder.timeTxt.setText(String.valueOf(hours) + "h");
+            } else if (days<31){
+                holder.timeTxt.setText(String.valueOf(days) + "d");
+            } else if (months<12){
+                holder.timeTxt.setText(String.valueOf(months) + "m" );
+            } else {
+                holder.timeTxt.setText(String.valueOf(years) + "y");
+            }
+
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTxt;
+        TextView messageTxt;
+        TextView timeTxt;
+        ImageView icon;
+        ImageView imageView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            messageTxt = itemView.findViewById(R.id.messageTxt);
+            icon = itemView.findViewById(R.id.icon);
+            imageView = itemView.findViewById(R.id.image);
+        }
+    }
+}
--- app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/NotificationActivity.java
+++ app/src/main/java/com/adityarana/sangharsh/learning/sangharsh/NotificationActivity.java
@@ -2,31 +2,38 @@ package com.adityarana.sangharsh.learning.sangharsh;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.adityarana.sangharsh.learning.sangharsh.Adapter.NotificationAdapter;
+import com.adityarana.sangharsh.learning.sangharsh.Model.Notifications.Notification;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
 public class NotificationActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
+    private ImageButton backBtn;
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
 
-    private void goBack(View view){
-        onBackPressed();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,13 @@ public class NotificationActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.progressBar);
         recyclerView = findViewById(R.id.recyclerView);
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         loadData();
     }
@@ -46,6 +60,42 @@ public class NotificationActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if (snapshot.exists()  && snapshot.hasChildren()){
+                            ArrayList<Notification> notifications = new ArrayList<Notification>();
+                            for (DataSnapshot x : snapshot.getChildren()) {
+                                Notification mNotif = x.getValue(Notification.class);
+                                if (mNotif.getExpiresIn() != null && !mNotif.getExpiresIn().isEmpty() && mNotif.getTime() != null
+                                        && mNotif.getTime().containsKey("createdAt")) {
+                                    try {
+                                        int expireIn = Integer.parseInt(mNotif.getExpiresIn());
+                                        Date date = new Date();
+                                        Date created = new Date();
+
+                                        long diff = date.getTime() - created.getTime();
+                                        int days = (int) (diff / (1000 * 60 * 60 * 24));
+
+                                        if (days < expireIn) {
+                                            notifications.add(mNotif);
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        notifications.add(mNotif);
+                                    }
+                                } else {
+                                    notifications.add(x.getValue(Notification.class));
+                                }
+                            }
+
+                            if (notifications.size() >0){
+                                Collections.reverse(notifications);
+                                NotificationAdapter adapter = new NotificationAdapter(notifications);
+                                recyclerView.setAdapter(adapter);
+                                recyclerView.setLayoutManager(new LinearLayoutManager(NotificationActivity.this));
+                                recyclerView.setVisibility(View.VISIBLE);
+                            } else {
+                                findViewById(R.id.noNotificationTxt).setVisibility(View.VISIBLE);
+                            }
+
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
 
